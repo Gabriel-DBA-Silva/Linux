c3237dc287aa550f56ee44cf445f618483bb9edf
Gabriel-DBA-Silva <145501087+Gabriel-DBA-Silva@users.noreply.github.com>
2023-09-27 17:34:20 -0300

    Update monitoramento.sh

--- monitoramento.sh
+++ monitoramento.sh
@@ -14,20 +14,20 @@ utildisk=$(sar -d 1 1 | grep %util | awk 'NR >1 {print $10}')
  
 # dados do tps para o diskA
 dadotps_A=$(sar -d 1 1 | awk 'NR==4 {print$4}')
-dadorkBs_A=$(sar -d 1 1 | awk 'NR==4 {print$5}')
-dadowkBs_A=$(sar -d 1 1 | awk 'NR==4 {print$6}')
+dadorkBs_A=$(sar -d 1 1 | awk 'NR==4 {print$5 * 1024 * 8}')
+dadowkBs_A=$(sar -d 1 1 | awk 'NR==4 {print$6} * 1024 * 8')
 dadoutil_A=$(sar -d 1 1 | awk 'NR==4 {print$11}')
 
 # dados do tps para o diskB
 dadotps_B=$(sar -d 1 1 | awk 'NR==5 {print$4}')
-dadorkBs_B=$(sar -d 1 1 | awk 'NR==5 {print$5}')
-dadowkBs_B=$(sar -d 1 1 | awk 'NR==5 {print$6}')
+dadorkBs_B=$(sar -d 1 1 | awk 'NR==5 {print$5 * 1024 * 8}')
+dadowkBs_B=$(sar -d 1 1 | awk 'NR==5 {print$6 * 1024 * 8}')
 dadoutil_B=$(sar -d 1 1 | awk 'NR==5 {print$11}')
 
 # dados do tps para o diskC
 dadotps_C=$(sar -d 1 1 | awk 'NR==6 {print$4}')
-dadorkBs_C=$(sar -d 1 1 | awk 'NR==6 {print$5}')
-dadowkBs_C=$(sar -d 1 1 | awk 'NR==6 {print$6}')
+dadorkBs_C=$(sar -d 1 1 | awk 'NR==6 {print$5} * 1024 * 8')
+dadowkBs_C=$(sar -d 1 1 | awk 'NR==6 {print$6 * 1024 * 8}')
 dadoutil_C=$(sar -d 1 1 | awk 'NR==6 {print$11}')
 
 #  distribuição do linux
@@ -42,7 +42,6 @@ ArqProc=$(uname -m)
 # tempo que a maquina está no ar
 #uptime=$(uptime | awk '{print $2, $3}')
 uptime_seconds=$(($(date +%s) - $(date -d "$(uptime -s)" +%s)))
-
 
 
 # memória: pgpgin/s)
@@ -62,37 +61,37 @@ majflts=$(sar -B 1 1 | awk 'NR==3 {print $6}')
 dadomajflts=$(sar -B 1 1 | awk 'NR==4 {print $6}')
 
 # memória (Men): total, used,free shared, buff/cache, available
-mem=$(free | awk 'NR==2 {print $1}')
+mem=$(free | awk 'NR==2 {gsub(/:/, "", $1); print $1}')
 
 total=$(free | awk 'NR==1 {print $1}')
-dadototal=$(free | awk 'NR==2 {print $2}')
+dadototal=$(free | awk 'NR==2 {print $2 * 1024}')
 
 # used
 used=$(free | awk 'NR==1 {print $2}')
-dadoused=$(free | awk 'NR==2 {print $3}')
+dadoused=$(free | awk 'NR==2 {print $3 * 1024}')
 
 #free 
 free=$(free | awk 'NR==1 {print $3}')
-dadofree=$(free | awk 'NR==2 {print $4}')
+dadofree=$(free | awk 'NR==2 {print $4} * 1024')
 #shared
 
 shared=$(free | awk 'NR==1 {print $4}')
-dadoshared=$(free | awk 'NR==2 {print $5}')
+dadoshared=$(free | awk 'NR==2 {print $5 * 1024}')
 
 # buff/cache
 buff_cache=$(free | awk 'NR==1 {print $5}')
-dadobuff_cache=$(free | awk 'NR==2 {print $6}')
+dadobuff_cache=$(free | awk 'NR==2 {print $6 * 1024}')
 
 # available
 available=$(free | awk 'NR==1 {print $6}')
-dadoavailable=$(free | awk 'NR==2 {print $7}')
+dadoavailable=$(free | awk 'NR==2 {print $7 * 1024}')
 
 # memória (Swap): swap-total, swap-used, swap-free
-swap=$(free | awk 'NR==3 {print $1}')
+swap=$(free | awk 'NR==3 {gsub(/:/, "", $1); print $1}')
 
-swap_total=$(free | awk 'NR==3 {print $2}')
-swap_used=$(free | awk 'NR==3 {print $3}')
-swap_free=$(free | awk 'NR==3 {print $4}')
+swap_total=$(free | awk 'NR==3 {print $2 * 1024}')
+swap_used=$(free | awk 'NR==3 {print $3 * 1024}')
+swap_free=$(free | awk 'NR==3 {print $4 * 1024}')
 
 # filesystem: Consumo em % de inodes
 #filesystem=$(df -i | awk 'NR==1 {print $1}')
@@ -106,13 +105,13 @@ filesystem=$(df -i | grep -v "tmpfs" | awk 'NR>1 {gsub(/%/, "", $5); print $NF "
 #tmpfs=$(df -i | awk 'NR==8 {print $1}')                
 
 # % de inodes
-Iiuse=$(df -i | awk 'NR==2 {print $5}')
-IIiuse=$(df -i | awk 'NR==3 {print $5}')
-IIIiuse=$(df -i | awk 'NR==4 {print $5}')
-IViuse=$(df -i | awk 'NR==5 {print $5}')
-Viuse=$(df -i | awk 'NR==6 {print $5}')
-VIiuse=$(df -i | awk 'NR==7 {print $5}')
-VIIiuse=$(df -i | awk 'NR==8 {print $5}')
+#Iiuse=$(df -i | awk 'NR==2 {print $5}')
+#IIiuse=$(df -i | awk 'NR==3 {print $5}')
+#IIIiuse=$(df -i | awk 'NR==4 {print $5}')
+#IViuse=$(df -i | awk 'NR==5 {print $5}')
+#Viuse=$(df -i | awk 'NR==6 {print $5}')
+#VIiuse=$(df -i | awk 'NR==7 {print $5}')
+#VIIiuse=$(df -i | awk 'NR==8 {print $5}')
 
 #  rede: rxpck/s, txpck/s, rxKb/s, txKb/s, %ifutil
 ## lo
@@ -120,14 +119,14 @@ lo=$(sar -n DEV 1 1 | awk 'NR==4 {print $3}')
 
 rxpcks=$(sar -n DEV 1 1 | awk 'NR==3 {print $4}') 
 txpcks=$(sar -n DEV 1 1 | awk 'NR==3 {print $5}') 
-rxKbs=$(sar -n DEV 1 1 | awk 'NR==3 {print $6}') 
-txKbs=$(sar -n DEV 1 1 | awk 'NR==3 {print $7}') 
+rxKbs=$(sar -n DEV 1 1 | awk 'NR==3 {print $6 * 1024 * 8}') 
+txKbs=$(sar -n DEV 1 1 | awk 'NR==3 {print $7 * 1024 * 8}') 
 ifutil=$(sar -n DEV 1 1 | awk 'NR==3 {print $11}')
 
 dadorxpcks=$(sar -n DEV 1 1 | awk 'NR==4 {print $4}') 
 dadotxpcks=$(sar -n DEV 1 1 | awk 'NR==4 {print $5}') 
-dadorxKbs=$(sar -n DEV 1 1 | awk 'NR==4 {print $6}') 
-dadotxKbs=$(sar -n DEV 1 1 | awk 'NR==4 {print $7}') 
+dadorxKbs=$(sar -n DEV 1 1 | awk 'NR==4 {print $6 * 1024 * 8}') 
+dadotxKbs=$(sar -n DEV 1 1 | awk 'NR==4 {print $7 * 1024 * 8}') 
 dadoifutil=$(sar -n DEV 1 1 | awk 'NR==4 {print $11}')
 
 ## enp0s3
@@ -135,8 +134,8 @@ enp0s3=$(sar -n DEV 1 1 | awk 'NR==5 {print $3}')
 
 endadorxpcks=$(sar -n DEV 1 1 | awk 'NR==5 {print $4}') 
 endadotxpcks=$(sar -n DEV 1 1 | awk 'NR==5 {print $5}') 
-endadorxKbs=$(sar -n DEV 1 1 | awk 'NR==5 {print $6}') 
-endadotxKbs=$(sar -n DEV 1 1 | awk 'NR==5 {print $7}') 
+endadorxKbs=$(sar -n DEV 1 1 | awk 'NR==5 {print $6 * 1024 * 8}') 
+endadotxKbs=$(sar -n DEV 1 1 | awk 'NR==5 {print $7 * 1024 * 8}') 
 endadoifutil=$(sar -n DEV 1 1 | awk 'NR==5 {print $11}')
 
 #estrutura do JSON para passar os dados de cada disco referente a 
